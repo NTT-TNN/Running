ba923ae85136e7df5fc19334ff1fda83349bd3c6
NguyenTienThao <nguyentienthao@Thaos-MacBook-Pro.local>
2018-02-08 22:38:54 +0700

    add score, disable collider plane, change color when collision sphere

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -1,86 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cube : MonoBehaviour {
+	public GameObject scoreObject;
+	int score=0;
 	float planeLengh = 5000; // độ dài của mỗi đường chạy khởi 
 	float plane_X = -1355;
 	float cube_Z;//ranger -450 -750
-	float cube_X=-1554;// range >-2000
+	float cube_X = -3000;// range >-3000
 	float cubeScale_Z;
 	float sphere_Z;//ranger -450 -750
 	float sphere_X=-1554;// range >-2000
 	float sphereScale_Z;
 	float moveSpeed = 900f;// tốc độ theo chiều ngang
-	float runSpeed = 600f; // tốc độ theo chiều 
+	float runSpeed = 400f; // tốc độ theo chiều 
 	float jumpHeight = 60f;
 	Color red = new Color (1, 0, 0, 1);
 	Color yellow = new Color (1, 0.92f, 0.016f, 1);
 	Color blue = new Color (0, 0, 1, 1);
+	Color playerColor;
+	public GameObject player;
 	// Use this for initialization
 	void Start () {
-		
-//		for(int i=0;i<100;++i) { // tao 100 cube chuong ngai 
-//			var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-//
-//			var boxCollider1 = cube.AddComponent<BoxCollider>();
-//			boxCollider1.center = new Vector3(0,0,0);
-//
-//			cube_Z = Random.Range(-750f, -450f); // chiều rộng của đường chạy
-//			float distanceCube_X = Random.Range(400, 700); // khoảng cách giữa các chướng ngại
-//			int flagColor=Random.Range(0,2);
-//			if (flagColor == 0) {
-//				cube.name = "red_cube";
-//				cube.GetComponent<Renderer> ().material.color = red;
-//			} else if (flagColor == 1) {
-//				cube.name = "blue_cube";
-//				cube.GetComponent<Renderer> ().material.color = blue;
-//			} else {
-//				cube.name = "yellow_cube";
-//				cube.GetComponent<Renderer>().material.color = yellow;
-//			}
-//				
-//
-//			cube.transform.position = new Vector3(cube_X+distanceCube_X, .5f, cube_Z);
-//			cube_X = cube_X + distanceCube_X;
-//			cubeScale_Z = Random.Range (50, 200);// độ rộng của chướng ngại 
-//			cube.transform.localScale = new Vector3 (20, 100, cubeScale_Z);
-//
-//			//them Sphere
-//			int flagSphere=Random.Range(1,2);
-//			if (flagSphere == 1) {
-//				var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-//				if (flagColor == 0) {
-//					sphere.name = "red_shere";
-//					sphere.GetComponent<Renderer> ().material.color = red;
-//				} else if (flagColor == 1) {
-//					sphere.name = "blue_shere";
-//					sphere.GetComponent<Renderer> ().material.color = blue;
-//				} else {
-//					sphere.name = "yellow_shere";
-//					sphere.GetComponent<Renderer>().material.color = yellow;
-//				}
-//
-//				var sphereCollider = (BoxCollider)sphere.AddComponent<BoxCollider>();
-//				sphereCollider.center = new Vector3(0,0,0);
-//
-//				cube_Z = Random.Range(-750f, -450f);
-//				float distanceShepre_X = Random.Range(400, 700);
-//				sphere.transform.position = new Vector3(cube_X+distanceShepre_X, .5f, cube_Z);
-//				sphere_X = sphere_X + distanceShepre_X;
-//				sphere.transform.localScale = new Vector3 (80, 80, 80);
-//			}
-//		}
-//
-//		//them plane
-//		var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-//		plane.GetComponent<Renderer>().material.color = new Color(0.5f,1,1);
-//		plane.transform.position = new Vector3(plane_X+planeLengh, 0, -591);
-//		plane_X = plane_X + planeLengh;
-//		plane.transform.localScale = new Vector3 (500, 2, 50);
-
-//		print ("hello world");
-
+		player = GameObject.Find("Player");
+		playerColor = player.GetComponent<Renderer> ().material.color;
+		print (playerColor);
 	}
 	
 //	 Update is called once per frame
@@ -93,10 +39,8 @@ public class Cube : MonoBehaviour {
 		if (currentPosition.y > 37.5) {
 			transform.position = new Vector3 (currentPosition.x + runSpeed* Time.deltaTime, (currentPosition.y-10f), currentPosition.z + runSpeed*Input.GetAxis("Vertical")* Time.deltaTime);
 		}
-//		print (currentPosition.y);
-//		print (currentPosition.z);
 
-		if (currentPosition.x > (cube_X - 2000)) {//nếu cube(player) đến cách cube cuối cùng 2000f thì sẽ tạo thêm cube ca thêm Sphere
+		if (currentPosition.x > (cube_X - 1000)) {//nếu cube(player) đến cách cube cuối cùng 2000f thì sẽ tạo thêm cube ca thêm Sphere
 			
 			for(int i=0;i<100;++i) {
 				//them cube
@@ -106,7 +50,7 @@ public class Cube : MonoBehaviour {
 
 				cube_Z = Random.Range(-750f, -450f);
 				float distanceCube_X = Random.Range(400, 700);
-				int flagColor=Random.Range(0,2);
+				int flagColor=Random.Range(0,3);
 				if (flagColor == 0) {
 					cube.name = "red_cube";
 					cube.GetComponent<Renderer> ().material.color = red;
@@ -119,20 +63,21 @@ public class Cube : MonoBehaviour {
 				}
 				cube.transform.position = new Vector3(cube_X+distanceCube_X, .5f, cube_Z);
 				cube_X = cube_X + distanceCube_X;
-				cube.transform.localScale = new Vector3 (20, 100, 200);
+				int cubeWidth = Random.Range (100, 200);
+				cube.transform.localScale = new Vector3 (20, 100, cubeWidth);
 				//them Sphere
 				int flagSphere=Random.Range(1,2);
 
 				if (flagSphere == 1) {
 					var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 					if (flagColor == 0) {
-						sphere.name = "red_shere";
+						sphere.name = "red_sphere";
 						sphere.GetComponent<Renderer> ().material.color = red;
 					} else if (flagColor == 1) {
-						sphere.name = "blue_shere";
+						sphere.name = "blue_sphere";
 						sphere.GetComponent<Renderer> ().material.color = blue;
 					} else {
-						sphere.name = "yellow_shere";
+						sphere.name = "yellow_sphere";
 						sphere.GetComponent<Renderer>().material.color = yellow;
 					}
 
@@ -141,19 +86,18 @@ public class Cube : MonoBehaviour {
 
 					cube_Z = Random.Range(-750f, -450f);
 					float distanceShepre_X = Random.Range(400, 700);
-					sphere.transform.position = new Vector3(cube_X+distanceShepre_X, .5f, cube_Z);
+					sphere.transform.position = new Vector3(sphere_X+distanceShepre_X, .5f, cube_Z);
 					sphere_X = sphere_X + distanceShepre_X;
 					sphere.transform.localScale = new Vector3 (80, 80, 80);
 				}
 			}
 
-
-
 		}
 		//them plane
 		if (currentPosition.x > (plane_X - 500)) {// nếu cube (player) đi gần hết plane thì thêm plane 
 			var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-			plane.GetComponent<Renderer>().material.color = new Color(1,1,1);
+			plane.GetComponent<MeshCollider> ().enabled = false; 
+			plane.GetComponent<Renderer>().material.color = new Color(0,1,1,1);
 			plane.transform.position = new Vector3(plane_X+planeLengh, 0, -591);
 			plane_X = plane_X + planeLengh;
 			plane.transform.localScale = new Vector3 (500, 2, 50);
@@ -170,12 +114,41 @@ public class Cube : MonoBehaviour {
 	void OnCollisionEnter(Collision collision)
 	{
 		print("collision" + collision.transform.name + "=> dead ");
-		moveSpeed = 0;
-		runSpeed = 0;
-		jumpHeight = 0; 
+		if (collision.transform.name == "blue_sphere" ) {
+			player.GetComponent<Renderer> ().material.color = blue;
+			playerColor = blue;
+			score+=15;
+			scoreObject.GetComponent<Text> ().text = score.ToString();// hien thi ra man hinh
+//			print ("change to blue");
+		} else if (collision.transform.name == "red_sphere") {
+			player.GetComponent<Renderer> ().material.color = red;
+			playerColor = red;
+			score+=15;
+			scoreObject.GetComponent<Text> ().text = score.ToString();
+//			print ("change to red");
+		} else if (collision.transform.name == "yellow_sphere" ) {
+			player.GetComponent<Renderer> ().material.color = yellow;
+			playerColor = yellow;
+			score+=15;
+			scoreObject.GetComponent<Text> ().text = score.ToString();
+//			print ("change to yellow");
+		} else if (collision.transform.name == "blue_cube" && playerColor == blue) {
+			score+=5;
+			scoreObject.GetComponent<Text> ().text = score.ToString();
+		} else if (collision.transform.name == "red_cube" && playerColor == red) {
+			score+=5;
+			scoreObject.GetComponent<Text> ().text = score.ToString();
+		} else if (collision.transform.name == "yellow_cube" && playerColor == yellow) {
+			score+=5;
+			scoreObject.GetComponent<Text> ().text = score.ToString();
+		} else {
+			moveSpeed = 0;
+			runSpeed = 0;
+			jumpHeight = 0;
+		}
 	}
 	void OnCollisionExit(Collision collisionInfo) {
-		print("Exit cube" + collisionInfo.transform.name);
+		print("Exit cube " + collisionInfo.transform.name);
 	}
 	void OnTriggerEnter(Collider collider)
 	{
